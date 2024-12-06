9d7c2c42c7494f2542e9a54c1cc0d4f0ca0b7cb8
GISwarm <gordeiegorov@gmail.com>
2024-12-06 05:41:53 +0300

    add maturity to stop uncontrollable reproducing

--- Main.gd
+++ Main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var circle_scene: PackedScene
-var spawn_interval: float = 1.0
+var spawn_interval: float = 3.0
 var spawn_timer: float = 0.0
 
 func _ready():
@@ -12,12 +12,15 @@ func _ready():
 func _process(delta):
 	spawn_timer -= delta
 	if spawn_timer <= 0:
-		spawn_circle()
+		spawn_random_circle()
 		spawn_timer = spawn_interval
 
-func spawn_circle():
-	var circle = circle_scene.instantiate() 
+func spawn_random_circle():
+	var circle = circle_scene.instantiate()
 	circle.position = Vector2(randf_range(0, get_viewport().size.x), randf_range(0, get_viewport().size.y))
 	circle.mass = randf_range(1.0, 5.0)     # Random weight between 1 and 5
 	circle.radius = randf_range(10, 30)      # Random radius between 10 and 30
+	spawn_circle(circle)
+
+func spawn_circle(circle):
 	add_child(circle)
--- circle.gd
+++ circle.gd
@@ -2,14 +2,17 @@ extends RigidBody2D
 
 var radius: float
 var velocity: Vector2
+var maturity: float
 
 func _ready():
 	mass = randf_range(0.2, 1.0)**2 * 1000
 	radius = mass
 	velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * mass
+	maturity = 2
 
 func _process(delta):
-	#position += direction * speed * delta
+	# mature
+	maturity -= delta
 	# Check for bounds: if outside of screen boundaries, reverse direction
 	if position.x < 0 or position.x > get_viewport().size.x:
 		velocity.x = -velocity.x
@@ -31,4 +34,8 @@ func handle_collision(body, angle):
 		body.queue_free()  # Remove the lighter circle
 	elif mass < body.mass:
 		queue_free()  # Remove this circle if it is lighter
-	get_parent().spawn_circle()
+	# merge collided circles
+	if maturity <= 0 and body.maturity <= 0:
+		var circle = get_parent().circle_scene.instantiate()
+		circle.velocity = (velocity + body.velocity) / 2
+		get_parent().spawn_circle(circle)
